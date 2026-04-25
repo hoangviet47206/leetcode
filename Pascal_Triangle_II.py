684778c1a0550935c3f1fe7e0f7c8d3b530cd42b
Nguyễn Hoàng Việt <24022843@vnu.edu.vn>
2026-04-25 22:28:13 +0700

    Create Pascal_Triangle_II.py

---
+++ Pascal_Triangle_II.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        res = []
+        for i in range(rowIndex + 1):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = res[i - 1][j - 1] + res[i - 1][j]
+            res.append(row)
+        return res[rowIndex]
