ab6a58da1902ee777b600a6672ee0a1ed9b3fa01
Nguyễn Hoàng Việt <24022843@vnu.edu.vn>
2026-08-06 14:11:44 +0700

    Create Flood_Fill.py

---
+++ Flood_Fill.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def floodFill(self, image, sr, sc, color):
+        """
+        :type image: List[List[int]]
+        :type sr: int
+        :type sc: int
+        :type color: int
+        :rtype: List[List[int]]
+        """
+        check = image[sr - 1][sc - 1]
+        for i in range(sr - 1, len(image)):
+            for j in range(sc - 1, 
+                           
+                           
+                           len(image[0])):
+                if check == image[i][j] :
+                    image[i][j] = color
+                else:
+                    break
+
+        return image
+def main():
+    sol = Solution()
+    image = [[0, 0, 0], [0, 0, 1]]
+    sr = 0
+    sc = 0
+    color = 2
+    result = sol.floodFill(image, sr, sc, color)
+    print(result)
+if __name__ == "__main__":
+    main()   
