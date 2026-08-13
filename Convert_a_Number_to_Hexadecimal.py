bddeb41e09e8237f6d09b6aa2718b952bddd3f24
Nguyễn Hoàng Việt <24022843@vnu.edu.vn>
2026-08-13 23:12:34 +0700

    Question 405

---
+++ Convert_a_Number_to_Hexadecimal.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def toHex(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        num = num if num >= 0 else num + 2**32
+        hexa = "0123456789abcdef"
+        result = ""
+        while (num > 0):
+            tmp = num % 16
+            result = hexa[tmp] + result
+            num = num // 16
+        
+        return result
+def main():
+    sol = Solution()
+    num = -1
+    result = sol.toHex(num)
+    print(result)
+if __name__ == "__main__":
+    main()
+            
+            
