4813e27111f7a01e5207ca1405e35e07ee3a5734
Nguyễn Hoàng Việt <24022843@vnu.edu.vn>
2026-08-16 10:18:59 +0700

    Question 521

--- Longest_Uncommon_Subsequence_I.py
+++ Longest_Uncommon_Subsequence_I.py
@@ -5,4 +5,20 @@ class Solution(object):
         :type b: str
         :rtype: int
         """
+        return max(self.diffchar(a, b), self.diffchar(b, a))
+    def diffchar(self, a, b):
+        dem = 0
+        for i in a:
+            for j in b:
+                if a != b:
+                    dem = dem + 1
+                    break
         
+        return dem
+
+def main():
+    sol = Solution()
+    result = sol.findLUSlength(a="ab", b="def")
+    print(result)
+if __name__ == "__main__":
+    main()
